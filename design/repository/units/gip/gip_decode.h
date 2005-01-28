d3d405786abd38f795f96d32edd6db011a8d6b78
gavin <>
2005-01-28 17:50:47 +0000

    Added simple ARM decode; some stuff works

--- design/repository/units/gip/gip_decode.h
+++ design/repository/units/gip/gip_decode.h
@@ -1,7 +1,5 @@
 extern module gip_decode_native( input bit[16] opcode,
 
-                                 input t_gip_ins_cc dec_cc,
-
                                  input bit[8] special_repeat_count,
                                  input bit[2] special_alu_mode,
 
@@ -29,7 +27,7 @@ extern module gip_decode_native( input bit[16] opcode,
 
     )
 {
-    timing comb input opcode, dec_cc;
+    timing comb input opcode;
     timing comb input special_repeat_count, special_alu_mode;
     timing comb output inst, pc_op, native_mapped_branch_offset;
 
@@ -38,4 +36,30 @@ extern module gip_decode_native( input bit[16] opcode,
 
     timing comb input in_conditional_shadow, in_immediate_conditional_shadow;
     timing comb output next_in_immediate_conditional_shadow;
+}
+
+extern module gip_decode_arm( input bit[32] opcode,
+                       input bit[5] cycle_of_opcode,
+
+                       output t_gip_instruction_rf inst,
+
+                       output bit[5] next_cycle_of_opcode,
+                       output t_gip_pc_op pc_op,
+                       output bit[32] arm_branch_offset,
+
+                       input bit extended,
+                       input bit[28] extended_immediate,
+                       input t_gip_ins_r extended_rd,
+                       input t_gip_ins_r extended_rn,
+                       input t_gip_ins_r extended_rm,
+                       input t_gip_ext_cmd extended_cmd
+
+                       )
+{
+    timing comb input opcode;
+
+    timing comb output inst, next_cycle_of_opcode, pc_op, arm_branch_offset;
+
+    timing comb input extended, extended_immediate, extended_rd, extended_rn, extended_rm, extended_cmd;
+
 }

048bdc6fe311e11a1b095093f38ff89f2b176d33
gavin <>
2005-08-09 20:26:31 +0000

    Added standard semaphore numbers

--- design/repository/headers/c/gip_system.h
+++ design/repository/headers/c/gip_system.h
@@ -10,6 +10,20 @@
 #define IO_A_SLOT_SYNC_SERIAL_0 (1)
 #define IO_A_SLOT_PARALLEL_0    (3)
 
+#define SEM_NUM_MICROKERNEL_TRAP    (4)
+#define SEM_NUM_MICROKERNEL_INT_EN  (5)
+#define SEM_NUM_MICROKERNEL_HW_INT  (6)
+#define SEM_NUM_STATUS_VNE  (24)
+#define SEM_NUM_TX_DATA_VWM (25)
+#define SEM_NUM_SOFT_IRQ    (26)
+#define SEM_NUM_POSTBUS     (27)
+#define SEM_NUM_TIMER       (28)
+
+#define SEM_STATUS_VNE  (1<<SEM_NUM_STATUS_VNE)
+#define SEM_TX_DATA_VWM (1<<SEM_NUM_TX_DATA_VWM)
+#define SEM_SOFT_IRQ    (1<<SEM_NUM_SOFT_IRQ)
+#define SEM_POSTBUS     (1<<SEM_NUM_POSTBUS)
+        
 /*a End
  */
 #endif  /* __GIP_SYSTEM */
